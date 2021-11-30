7da424fbdf057178fd3841f407431cede61b1c7e
Ihmahmoud <77974878+Ihmahmoud@users.noreply.github.com>
2021-11-30 22:18:49 +0300

    5-Build Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,32 @@
 import React from 'react';
-import Product from './components/Product';
-import data from './data';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
-    <div className="grid-container">
+    <BrowserRouter>
+      <div className="grid-container">
         <header className="row">
-            <div>
-                <a className="brand" href="/">amazona</a>
-            </div>
-            <div>
-                <a href="/card">Cart</a>
-                <a href="/signin">Sign In</a>
-            </div>
+          <div>
+            <a className="brand" href="/">amazona</a>
+          </div>
+          <div>
+            <a href="/card">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
         </header>
         <main>
-          <div>
-            <div className="row center">
-              {
-                data.products.map(product =>(
-                  <Product key={product._id} product={product}></Product>
-                ))
-              }
-            </div>
-          </div>
+        
+          <Routes>
+          {/* <Route Path="/product/:id" component={ProductScreen}></Route> */}
+            <Route path="/" element={<HomeScreen/>} exact/>
+            {/* <Route path="/" component={HomeScreen} exact></Route> */}
+            <Route path="/product/:id" element={<ProductScreen/>}/>
+          </Routes>
         </main>
         <footer className="row center">All right reserved</footer>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
